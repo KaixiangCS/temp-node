d9ade7f392ba3921eeec3411eb0eb127dc217842
Kaixiang Zhang <kaixiangzhang@Kaixiangs-MacBook-Air.local>
2022-06-16 19:53:16 -0700

    first commit

---
+++ 03-modules.js
@@ -0,0 +1,6 @@
+const {john,peter} = require('./04-names');
+const sayHi = require('./05-utils');
+require('./07-mind')
+//sayHi('susan')
+//sayHi(john)
+//sayHi(peter)
---
+++ 04-names.js
@@ -0,0 +1,7 @@
+const john = 'john'
+const peter = 'peter'
+const secret = 'top secret'
+
+module.exports = {john,peter}
+//console.log(module)
+
